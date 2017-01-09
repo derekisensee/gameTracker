30de12a7ddc79f61ddcce5656708ba3672183831
Derek Isensee <derek.isensee@gmail.com>
2017-01-09 09:05:39 -0600

    Remove unneeded import

--- src/com/derek/gameTracker/gameSubmitGUI.java
+++ src/com/derek/gameTracker/gameSubmitGUI.java
@@ -2,7 +2,6 @@
 package com.derek.gameTracker;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 import java.awt.*;
